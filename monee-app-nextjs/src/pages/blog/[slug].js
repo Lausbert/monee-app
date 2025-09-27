46ad85eb8ba9c40f4ef0e30d33743b7a9bb17b4d
Ronald-Bonald <brumm3@gmail.com>
2025-09-28 00:26:55 +0200

    minor

--- monee-app-nextjs/src/pages/blog/[slug].js
+++ monee-app-nextjs/src/pages/blog/[slug].js
@@ -62,7 +62,7 @@ export default function BlogPost({ post, content, allTranslations, commonTransla
         <meta property="og:type" content="article" />
         <meta property="og:title" content={`${title} | ${translatedAppName}`} />
         <meta property="og:description" content={description} />
-        <meta property="og:image" content={post.meta.authorImage} />
+        <meta property="og:image" content={toAbsoluteUrl(post.meta.authorImage)} />
         <meta property="og:image:alt" content={post.meta.title} />
         <meta property="og:url" content={`https://monee-app.com/${locale !== 'en' ? locale + '/' : ''}blog/${post.slug}/`} />
         <meta property="og:site_name" content={translatedAppName} />
@@ -74,10 +74,8 @@ export default function BlogPost({ post, content, allTranslations, commonTransla
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:title" content={`${title} | ${translatedAppName}`} />
         <meta name="twitter:description" content={description} />
-        <meta name="twitter:image" content={post.meta.authorImage} />
+        <meta name="twitter:image" content={toAbsoluteUrl(post.meta.authorImage)} />
         <meta name="twitter:image:alt" content={post.meta.title} />
-        <meta name="twitter:site" content="@MoneeApp" />
-        <meta name="twitter:creator" content="@MoneeApp" />
         {/* Canonical URL */}
         <link rel="canonical" href={`https://monee-app.com/${locale !== 'en' ? locale + '/' : ''}blog/${post.slug}/`} />
         {/* Language alternates with translated slugs */}
